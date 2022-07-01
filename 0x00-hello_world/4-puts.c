21ae047a03ea1b118b8bca6dfaa142eccc402458
Abodunde <aboduntobi@gmail.com>
2022-07-01 14:13:48 -0700

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-int puts(Programming is like building a multilingual puzzle)
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
