4b986cfe32f7d1221b5481549387414dfa5ec10e
Abodunde <aboduntobi@gmail.com>
2022-07-04 09:15:11 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,25 +11,28 @@ int main(void)
 
 	for (j = 0; j <= 99; j++)
 	{
-	for  (i = j; i <= 99; i++)
+		for  (i = j; i <= 99; i++)
+		{
+			if (i != j)
 			{
-				if (i != j)
-				{
-					putchar(j / 10 + 48);
-					putchar(j % 10 + 48);
-					putchar(' ');
-					putchar(i / 10 + 48);
-					putchar(i % 10 + 48);
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+				putchar(' ');
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
 
-					if (j * 100 + i != 9899)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				if (j * 100 + i != 9899)
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
 
 		}
-		putchar('\n');
-		return (0);
+
+
 	}
+	putchar('\n');
+	return (0);
+
+}
