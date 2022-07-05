b417684e0711b7e16885ee14e034559c12c44c1e
Abodunde <aboduntobi@gmail.com>
2022-07-05 08:54:08 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Functions print the alphabet in lowercase.
+ *
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
