80bbacd38ab100f388a1b4c15de4c7cf29ae950d
Abodunde <aboduntobi@gmail.com>
2022-07-05 09:13:07 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,9 @@
 /**
  * _islower -Function checks islower character
  *
- * Return: Always 0 (Success)
+ * @c: character to be checked
+ *
+ * Return: 1 if c is lowercase 0 otherwise
  */
 
 int _islower(int c)
