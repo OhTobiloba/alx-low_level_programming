160709e9242639b016d0b191b8320902c1107d1e
Abodunde <aboduntobi@gmail.com>
2022-07-05 09:02:29 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Functions print the alphabet in lower case.
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+	}
+	_putchar('\n');
+}
