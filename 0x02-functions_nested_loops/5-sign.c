01881ff05fe8593bde4a05af24c1d1b831baec9e
Abodunde <aboduntobi@gmail.com>
2022-07-05 09:25:18 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * 
+ * @c: number to check
+ *
+ * Return: +1 if is positive, 0 if is 0 and -1 if is negative
+ */
+
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
