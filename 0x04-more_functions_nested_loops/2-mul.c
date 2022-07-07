4b6c8bcd3110e1ac1abd7bc0221b7fd701e8de00
Abodunde <aboduntobi@gmail.com>
2022-07-07 08:21:28 -0700

    Function that multiples two input

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiples two integers.
+ * @: An input integer
+ * @b: An input integer
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
