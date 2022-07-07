aceee447ccc085c893ff8e9846d846012e3d4f58
Abodunde <aboduntobi@gmail.com>
2022-07-07 10:40:38 -0700

    changed

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
 
 /**
  * main - Entry point
- * return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long int i = 3, n = 612852475143;
+	unsigned long int i =3, n = 612852475143;
 
 	for (; i < 12057; i += 2)
 	{
-		while (n % i == 0 && n != i)
-			n /= i;
+	while (n % i == 0 && n != i)
+	n /= i;
 	}
-	printf("%lu\n", n);
-	return (0);
+printf("%lu\n", n);
+return (0);
 }
