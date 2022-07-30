d38eaf675289804786fb65d9dbcf0c01214993b6
Abodunde <aboduntobi@gmail.com>
2022-07-29 18:15:05 -0700

    new file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
-void _puts(char *str);
 
 #endif
