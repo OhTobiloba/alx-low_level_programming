eefa52b1123eda2e529c2eff59d8508b65ff51b7
Abodunde <aboduntobi@gmail.com>
2022-08-15 18:03:07 -0700

    new changes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,20 +1,31 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _LISTS_
+#define _LISTS_
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  * for Holberton project
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
-size_t recursion_printer(const listint_t *h);
+
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ * Description: singly linked list of pointer
+ */
+typedef struct listp_s
+{
+void *p;
+struct listp_s *next;
+} listp_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -29,7 +40,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-void free_list(listint_t *head);
-void free_listp(listp_t **head);
 listint_t *find_listint_loop(listint_t *head);
-#endif /* _LISTS_H_ */
+#endif
