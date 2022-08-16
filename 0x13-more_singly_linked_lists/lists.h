424d42de4e677ac4d67262e214904986ffd8aabb
Abodunde <aboduntobi@gmail.com>
2022-08-15 17:57:48 -0700

    new file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,5 +30,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 void free_list(listint_t *head);
+void free_listp(listp_t **head);
 listint_t *find_listint_loop(listint_t *head);
 #endif /* _LISTS_H_ */
